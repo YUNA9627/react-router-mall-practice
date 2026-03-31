37994d59c9c0968e552f4931358d99625c19cbe4
YUNA9627 <yuna2175@naver.com>
2026-03-31 23:13:11 +0900

    내비게이션 구조 수정

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,46 +1,63 @@
 import React, { useState } from "react";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 
 const Navbar = () => {
   const menuList = ["SHOP", "COLLECTION", "STYLE", "MORE"];
   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
-    <header className="navbar">
-      <div className="top-section">
-        <div className="logo-area">
-          <img width={200} src="../src/assets/logo.png" alt="logo" />
-        </div>
+    <>
+      <header>
+        <div className="navbar">
+          <div className="top-section">
+            <div className="logo-area">
+              <img width={200} src="../src/assets/logo.png" alt="logo" />
+            </div>
 
-        <div className="menu-area">
-          <ul className="menu-list">
-            {menuList.map((menu) => (
-              <li key={menu}>{menu}</li>
-            ))}
-          </ul>
-        </div>
+            <div className="menu-area">
+              <ul className="menu-list">
+                {menuList.map((menu) => (
+                  <li key={menu}>{menu}</li>
+                ))}
+              </ul>
+            </div>
 
-        <div className="right">
-          <button
-            className="top-search-button"
-            onClick={() => setIsSearchOpen((prev) => !prev)}
-          >
-            <Search size={20} />
-          </button>
-          <div className="login-button">LOGIN</div>
-        </div>
-      </div>
+            <div className="right">
+              <button
+                className="top-search-button"
+                onClick={() => setIsSearchOpen((prev) => !prev)}
+              >
+                <Search size={20} />
+              </button>
 
-      {isSearchOpen && (
-        <div className="search-section">
-          <div className="search-bar">
-            <Search size={18} className="search-icon" />
-            <input type="text" placeholder="Search for something" />
-            <button className="search-button">SEARCH</button>
+              <div className="login-button">LOGIN</div>
+            </div>
           </div>
+
+          {isSearchOpen && (
+            <>
+              <div
+                className="search-overlay"
+                onClick={() => setIsSearchOpen(false)}
+              ></div>
+              <div className="search-section">
+                <div className="search-bar">
+                  <Search size={20} />
+                  <input type="text" placeholder="Search for something" />
+                  <button className="search-button">SEARCH</button>
+                </div>
+              </div>
+            </>
+          )}
         </div>
-      )}
-    </header>
+        <div className="banner">
+          <img
+            src="https://storage.heypop.kr/assets/2023/08/24165850/t-8.jpg"
+            alt=""
+          />
+        </div>
+      </header>
+    </>
   );
 };
 
