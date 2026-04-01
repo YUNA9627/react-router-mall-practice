d36f1c1d74c4abcd13de6450f71e867f15cd4c15
YUNA9627 <yuna2175@naver.com>
2026-04-02 00:01:18 +0900

    feat: choice 상품 목록 필터링 추가

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -15,17 +15,21 @@ const ProductAll = () => {
   }, []);
 
   return (
-    <div>
-      <Container>
-        <Row>
-          {productList.map((menu) => (
-            <Col lg={3}>
-              <ProductCard item={menu} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
+    <>
+      <div>
+        <Container>
+          <Row>
+            {productList
+              .filter((item) => item.choice === true)
+              .map((menu) => (
+                <Col lg={3}>
+                  <ProductCard item={menu} />
+                </Col>
+              ))}
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 };
 
