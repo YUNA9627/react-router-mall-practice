ffa6eee5c508980e8f959a8f83b318adc5152ae4
YUNA9627 <yuna2175@naver.com>
2026-04-02 00:03:15 +0900

    feat: 로그인일 때 배너 숨기기 기능 추가

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,108 +1,115 @@
 import React, { useState } from "react";
 import { Search, Menu, X } from "lucide-react";
 import logo from "../assets/logo.png";
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
 
 const Navbar = () => {
   const menuList = ["SHOP", "COLLECTION", "STYLE", "MORE"];
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const hideBanner = location.pathname === "/login";
 
   const goToLogin = () => {
-    navigate("/login")
-  }
+    navigate("/login");
+  };
 
   return (
     <>
       <header>
-        <div className="nav-section">
-          <div className="top-section">
-            <div className="left">
-              <button
-                className="hamburger-button"
-                onClick={() => setIsMenuOpen(true)}
-              >
-                <Menu size={22} />
-              </button>
-            </div>
-            <div className="logo-area">
-              <a href="" onClick={() => window.location.reload()}>
-                <img className="logo" src={logo} alt="logo" />
-              </a>
-            </div>
-
-            <div className="menu-area">
-              <ul className="menu-list">
-                {menuList.map((menu) => (
-                  <li key={menu}>{menu}</li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="right">
-              <button
-                className="top-search-button"
-                onClick={() => setIsSearchOpen((prev) => !prev)}
-              >
-                <Search size={20} />
-              </button>
-
-              <div className="login-button" onClick={goToLogin}>LOGIN</div>
-            </div>
-          </div>
-
-          {isSearchOpen && (
-            <>
-              <div
-                className="search-overlay"
-                onClick={() => setIsSearchOpen(false)}
-              ></div>
-              <div className="search-section">
-                <div className="search-bar">
-                  <Search size={18} />
-                  <input type="text" placeholder="Search for something" />
-                  <button className="search-button">SEARCH</button>
-                </div>
+          <div className="nav-section">
+            <div className="top-section">
+              <div className="left">
+                <button
+                  className="hamburger-button"
+                  onClick={() => setIsMenuOpen(true)}
+                >
+                  <Menu size={22} />
+                </button>
               </div>
-            </>
-          )}
+              <div className="logo-area">
+                <a href="" onClick={() => window.location.reload()}>
+                  <img className="logo" src={logo} alt="logo" />
+                </a>
+              </div>
 
-          {isMenuOpen && (
-            <>
-              <div
-                className="menu-overlay"
-                onClick={() => setIsMenuOpen(false)}
-              ></div>
-
-              <aside className="side-menu">
-                <div className="side-menu-header">
-                  <button
-                    className="close-menu-button"
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    <X size={22} />
-                  </button>
-                </div>
-
-                <ul className="side-menu-list">
+              <div className="menu-area">
+                <ul className="menu-list">
                   {menuList.map((menu) => (
-                    <li key={menu}>
-                      <a href="/">{menu}</a>
-                    </li>
+                    <li key={menu}>{menu}</li>
                   ))}
                 </ul>
-              </aside>
-            </>
-          )}
-        </div>
-        <div className="banner">
-          <img
-            src="https://storage.heypop.kr/assets/2023/08/24165850/t-8.jpg"
-            alt=""
-          />
-        </div>
+              </div>
+
+              <div className="right">
+                <button
+                  className="top-search-button"
+                  onClick={() => setIsSearchOpen((prev) => !prev)}
+                >
+                  <Search size={20} />
+                </button>
+
+                <div className="login-button" onClick={goToLogin}>
+                  LOGIN
+                </div>
+              </div>
+            </div>
+
+            {isSearchOpen && (
+              <>
+                <div
+                  className="search-overlay"
+                  onClick={() => setIsSearchOpen(false)}
+                ></div>
+                <div className="search-section">
+                  <div className="search-bar">
+                    <Search size={18} />
+                    <input type="text" placeholder="Search for something" />
+                    <button className="search-button">SEARCH</button>
+                  </div>
+                </div>
+              </>
+            )}
+
+            {isMenuOpen && (
+              <>
+                <div
+                  className="menu-overlay"
+                  onClick={() => setIsMenuOpen(false)}
+                ></div>
+
+                <aside className="side-menu">
+                  <div className="side-menu-header">
+                    <button
+                      className="close-menu-button"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <X size={22} />
+                    </button>
+                  </div>
+
+                  <ul className="side-menu-list">
+                    {menuList.map((menu) => (
+                      <li key={menu}>
+                        <a href="/">{menu}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </aside>
+              </>
+            )}
+          </div>
+        {!hideBanner && (
+            <div className="banner">
+              <img
+                src="https://storage.heypop.kr/assets/2023/08/24165850/t-8.jpg"
+                alt=""
+              />
+            </div>
+        )}
       </header>
     </>
   );
