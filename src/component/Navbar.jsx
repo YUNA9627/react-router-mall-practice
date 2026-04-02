8679541d17a17fba842a2050a0884a5382755a93
YUNA9627 <yuna2175@naver.com>
2026-04-02 22:40:31 +0900

    feat: 엔터 입력 시 검색 기능 추가

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -19,99 +19,110 @@ const Navbar = () => {
   const goToHome = () => {
     navigate("/");
   };
-
+  const search = (event) => {
+    if (event.key === "Enter") {
+      // 입력한 검색어를 읽어와서
+      let keyword = event.target.value;
+      // url을 바꿔준다
+      navigate(`/?q=${keyword}`);
+    }
+  };
   return (
     <>
       <header>
-          <div className="nav-section">
-            <div className="top-section">
-              <div className="left">
-                <button
-                  className="hamburger-button"
-                  onClick={() => setIsMenuOpen(true)}
-                >
-                  <Menu size={22} />
-                </button>
-              </div>
-              <div className="logo-area">
-                <a href="" onClick={goToHome}>
-                  <img className="logo" src={logo} alt="logo" />
-                </a>
-              </div>
+        <div className="nav-section">
+          <div className="top-section">
+            <div className="left">
+              <button
+                className="hamburger-button"
+                onClick={() => setIsMenuOpen(true)}
+              >
+                <Menu size={22} />
+              </button>
+            </div>
+            <div className="logo-area">
+              <a href="" onClick={goToHome}>
+                <img className="logo" src={logo} alt="logo" />
+              </a>
+            </div>
 
-              <div className="menu-area">
-                <ul className="menu-list">
+            <div className="menu-area">
+              <ul className="menu-list">
+                {menuList.map((menu) => (
+                  <li key={menu}>{menu}</li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="right">
+              <button
+                className="top-search-button"
+                onClick={() => setIsSearchOpen((prev) => !prev)}
+              >
+                <Search size={20} />
+              </button>
+
+              <div className="login-button" onClick={goToLogin}>
+                LOGIN
+              </div>
+            </div>
+          </div>
+
+          {isSearchOpen && (
+            <>
+              <div
+                className="search-overlay"
+                onClick={() => setIsSearchOpen(false)}
+              ></div>
+              <div className="search-section">
+                <div className="search-bar">
+                  <Search size={18} />
+                  <input
+                    type="text"
+                    onKeyDown={(event) => search(event)}
+                    placeholder="Search for something"
+                  />
+                  <button className="search-button">SEARCH</button>
+                </div>
+              </div>
+            </>
+          )}
+
+          {isMenuOpen && (
+            <>
+              <div
+                className="menu-overlay"
+                onClick={() => setIsMenuOpen(false)}
+              ></div>
+
+              <aside className="side-menu">
+                <div className="side-menu-header">
+                  <button
+                    className="close-menu-button"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    <X size={22} />
+                  </button>
+                </div>
+
+                <ul className="side-menu-list">
                   {menuList.map((menu) => (
-                    <li key={menu}>{menu}</li>
+                    <li key={menu}>
+                      <a href="/">{menu}</a>
+                    </li>
                   ))}
                 </ul>
-              </div>
-
-              <div className="right">
-                <button
-                  className="top-search-button"
-                  onClick={() => setIsSearchOpen((prev) => !prev)}
-                >
-                  <Search size={20} />
-                </button>
-
-                <div className="login-button" onClick={goToLogin}>
-                  LOGIN
-                </div>
-              </div>
-            </div>
-
-            {isSearchOpen && (
-              <>
-                <div
-                  className="search-overlay"
-                  onClick={() => setIsSearchOpen(false)}
-                ></div>
-                <div className="search-section">
-                  <div className="search-bar">
-                    <Search size={18} />
-                    <input type="text" placeholder="Search for something" />
-                    <button className="search-button">SEARCH</button>
-                  </div>
-                </div>
-              </>
-            )}
-
-            {isMenuOpen && (
-              <>
-                <div
-                  className="menu-overlay"
-                  onClick={() => setIsMenuOpen(false)}
-                ></div>
-
-                <aside className="side-menu">
-                  <div className="side-menu-header">
-                    <button
-                      className="close-menu-button"
-                      onClick={() => setIsMenuOpen(false)}
-                    >
-                      <X size={22} />
-                    </button>
-                  </div>
-
-                  <ul className="side-menu-list">
-                    {menuList.map((menu) => (
-                      <li key={menu}>
-                        <a href="/">{menu}</a>
-                      </li>
-                    ))}
-                  </ul>
-                </aside>
-              </>
-            )}
-          </div>
+              </aside>
+            </>
+          )}
+        </div>
         {!hideBanner && (
-            <div className="banner">
-              <img
-                src="https://storage.heypop.kr/assets/2023/08/24165850/t-8.jpg"
-                alt=""
-              />
-            </div>
+          <div className="banner">
+            <img
+              src="https://storage.heypop.kr/assets/2023/08/24165850/t-8.jpg"
+              alt=""
+            />
+          </div>
         )}
       </header>
     </>
