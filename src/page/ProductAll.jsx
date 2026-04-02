18288ab2ca1f0a6606f477be2a70530ded25fdc6
YUNA9627 <yuna2175@naver.com>
2026-04-02 23:02:06 +0900

    feat: 검색어 query params 반영

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../component/ProductCard";
 import { Container, Row, Col } from "react-bootstrap";
+import { useSearchParams } from "react-router";
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
   const getProducts = async () => {
-    let url = `https://my-json-server.typicode.com/YUNA9627/react-router-mall-practice/products`;
+    let searchQuery = query.get('q') || "";
+    console.log(searchQuery)
+    let url = `https://my-json-server.typicode.com/YUNA9627/react-router-mall-practice/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
   };
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [query]);
 
   return (
     <>
